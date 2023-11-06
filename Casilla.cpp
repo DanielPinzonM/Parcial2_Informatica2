ecc2a7736cfcc5848481d73deb086ca6fd19fdf3
Daniel Pinzón <daniele.pinzon@udea.edu.co>
2023-11-05 22:31:48 -0500

    Implementación de la clase Casilla con su respectiva modularidad

---
+++ Casilla.cpp
@@ -0,0 +1,33 @@
+#include "Casilla.h"
+
+Casilla::Casilla(Contenido Cont) : Cont(Cont) {}
+
+void Casilla::setContenido(Contenido NuevoContenido) {
+        Cont = NuevoContenido;
+    }
+
+Contenido Casilla::getContenido(){
+        return Cont;
+    }
+
+bool Casilla::NoOcupada(){
+        return (Cont == Contenido::NoOcupada);
+    }
+
+char Casilla::RepresentarContenido(){
+
+        switch (Cont) {
+
+        case Contenido::FichaBlanca:
+            return 'o';
+
+        case Contenido::FichaNegra:
+            return '+';
+            
+        case Contenido::PosibleJugada:
+            return '_';
+            
+        default:
+            return '.';
+        }
+    }
